a5d5036bb721b30ab06255d188883f95c3f51891
orenovadia <orenovad@gmail.com>
2020-10-29 12:50:16 -0700

    lexer kind of working

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,43 +1,147 @@
+use string_builder::Builder;
+
+use crate::lexer::Type::Identifier;
+
 #[derive(Debug)]
 #[derive(PartialEq)]
-pub struct Token {}
-
-pub struct Lexer {
+pub struct Token {
+    token_type: Type,
     text: String,
-    pos: u32,
+}
+
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub enum Type {
+    Dot,
+    Identifier,
+}
+
+#[derive(Debug)]
+pub struct Lexer {
+    chars: Vec<char>,
+    pos: usize,
+    total: usize,
 }
 
 impl Lexer {
-    pub fn on(text: String) -> Lexer
-    {
-        Lexer { text, pos: 0 }
+    pub fn on(text: String) -> Lexer {
+        let chars: Vec<char> = text.chars().collect();
+        let total = chars.len();
+        Lexer { chars, pos: 0, total }
     }
-    pub fn next_token(self: Self) -> Option<Token> {
-        Option::None
+
+    pub fn next_token(&mut self) -> Option<Token> {
+        let tot = self.total;
+        if self.pos == self.total {
+            None
+        } else { self.remaining() }
+    }
+
+    fn remaining(&mut self) -> Option<Token> {
+        while self.has_remaining() {
+            let next = self.advance();
+            println!("{}", next);
+
+            return match next {
+                '.' => Some(Token { token_type: Type::Dot, text: ".".to_string() }),
+                'a'..='z' | 'A'..='Z' => Some(Token { token_type: Identifier, text: self.consume_alnum(next) }),
+                _ => None
+            };
+        }
+        unreachable!()
+    }
+
+    fn consume_alnum(&mut self, first: char) -> String {
+        let mut builder = Builder::new(1);
+        builder.append(first);
+
+        let mut done = false;
+        while self.has_remaining() && !done {
+            let peek = self.peek();
+
+            match peek {
+                None => done = true,
+                Some(c) => {
+                    if c.is_alphanumeric() {
+                        builder.append(c);
+                    } else { done = true; }
+                }
+            }
+            self.advance();
+        }
+        return builder.string().unwrap();
+    }
+
+    fn has_remaining(&self) -> bool {
+        self.pos < self.total
+    }
+
+    fn advance(&mut self) -> char {
+        assert!(self.has_remaining());
+        let c = self.peek().unwrap();
+        self.pos += 1;
+        c
+    }
+
+    fn peek(&self) -> Option<char> {
+        self.chars.get(self.pos).map(|c| c.clone())
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::lexer::{Lexer, Token};
+    use crate::lexer::Type::{Dot, Identifier};
 
     #[test]
     fn lex_empty() {
         assert_no_token("")
     }
 
+    #[test]
+    fn test_dot() {
+        assert_tokenized(".", Token { token_type: Dot, text: ".".to_string() });
+    }
+
+    #[test]
+    fn test_identifier() {
+        assert_tokenized("bar", Token { token_type: Identifier, text: "bar".to_string() });
+    }
+
+    #[test]
+    fn test_identifier_with_numbers() {
+        assert_tokenized("a3b42", Token { token_type: Identifier, text: "a3b42".to_string() });
+    }
+
+    #[test]
+    fn test_dot_identifier() {
+        let dot = Token { token_type: Dot, text: ".".to_string() };
+        let ident = Token { token_type: Identifier, text: "bar".to_string() };
+        assert_tokenized_many(".bar", vec![dot, ident]);
+    }
+
     fn assert_no_token(text: &str) {
         let actual = first_token(text.to_string());
-        assert_eq!(None, actual)
+        assert_eq!(actual, None)
     }
 
     fn assert_tokenized(text: &str, token: Token) {
         let actual = first_token(text.to_string());
-        assert_eq!(Some(token), actual)
+        assert_eq!(actual, Some(token))
+    }
+
+    fn assert_tokenized_many(text: &str, tokens: Vec<Token>) {
+        let mut lex = Lexer::on(text.to_string());
+        for t in tokens {
+            assert_eq!(lex.next_token(), Some(t))
+        }
+        assert!(lex.next_token().is_none())
     }
 
     fn first_token(text: String) -> Option<Token> {
-        let lex = Lexer::on(text);
+        let mut lex = Lexer::on(text);
+        println!("{:#?}", lex);
+
         let actual = lex.next_token();
         actual
     }
